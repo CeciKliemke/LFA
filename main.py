e874864b997b16941d23c6dfd8e06f5383952609
CeciKliemke <cecikliemke@hotmail.com>
2023-12-05 21:35:46 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,89 @@
+import re
+import os
+
+
+diretorio_atual = os.path.dirname(os.path.abspath(__file__))
+
+caminho_arquivo = os.path.join(diretorio_atual, 'nome_do_arquivo.txt')
+
+def ler_arquivo(caminho_arquivo):
+    with open(caminho_arquivo, 'r') as arquivo:
+        return arquivo.read()
+def automato_atribuicao(expressao):
+    # Definindo estados
+    estados = {
+        'q0': 0,  # Estado inicial
+        'q1': 1,  # Identificador
+        'q2': 2,  # Operador de atribuição '='
+        'q3': 3,  # Número
+        'q4': 4,  # Operador aritmético
+        'q5': 5   # Estado final
+    }
+
+    # Definindo transições
+    transicoes = {
+        0: {'letra': 1},
+        1: {'letra': 1, 'digito': 1, '_': 1, 'espaco': 2},
+        2: {'=': 3},
+        3: {'digito': 4, 'letra': 4},
+        4: {'digito': 4, 'letra': 4, 'op_arit': 4, 'pv': 5, 'espaco': 5},
+    }
+
+    # Inicializando estado atual
+    estado_atual = estados['q0']
+
+    # Processando a expressão caractere por caractere
+    for char in expressao:
+        if char.isalpha():
+            categoria = 'letra'
+        elif char.isdigit():
+            categoria = 'digito'
+        elif char == '_':
+            categoria = '_'
+        elif char == '=':
+            categoria = '='
+        elif char in ('+', '-', '*', '/'):
+            categoria = 'op_arit'
+        elif char == ';':
+            categoria = 'pv'
+        elif char.isspace():
+            categoria = 'espaco'
+        else:
+            raise ValueError(f"Caractere inválido na expressão: {char}")
+
+        # Verificando transições
+        if categoria not in transicoes[estado_atual]:
+            raise ValueError(f"Transição inválida do estado {estado_atual} com o caractere {char}")
+
+        estado_atual = transicoes[estado_atual][categoria]
+
+    # Verificando se o estado final foi alcançado
+    return estado_atual == estados['q5']
+
+def avaliar_atribuicao(expressao):
+    # Separando a expressão em linhas
+    linhas = expressao.split('\n')
+
+    # Processando as linhas iniciais para definir os valores dos identificadores
+    identificadores = {}
+    for linha in linhas[:-2]:
+        nome, valor = linha.split('=')
+        identificadores[nome.strip()] = float(valor)
+
+    # Avaliando a expressão de atribuição
+    atribuicao = linhas[-2]
+    if automato_atribuicao(atribuicao):
+        # Substituindo os identificadores pelos valores
+        for nome, valor in identificadores.items():
+            atribuicao = re.sub(rf'\b{nome}\b', str(valor), atribuicao)
+
+        # Avaliando a expressão e imprimindo o resultado
+        resultado = eval(atribuicao)
+        print(f"A atribuição está correta. Resultado: {resultado}")
+    else:
+        print("A atribuição está incorreta.")
+
+# Exemplo de uso
+caminho_arquivo = 'caminho/do/seu/arquivo.txt'
+expressao_do_arquivo = ler_arquivo(caminho_arquivo)
+avaliar_atribuicao(expressao_do_arquivo)
